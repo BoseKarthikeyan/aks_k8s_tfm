fa333eadd0fc481246b077ec6ed0a0a72ef3b45f
BoseKarthikeyan <malkarthi@gmail.com>
2023-08-26 15:19:30 +0530

    Add files via upload

---
+++ examples/main.tf
@@ -0,0 +1,9 @@
+module "aks_k8s" {
+source = "../"
+resource_group_location = "eastus"
+resource_group_name_prefix = "rg"
+node_count = 2
+username = "karthi-azure"
+cluster_name = "karthik_test_cluster"
+node_pool_name = "nodejs"
+}
